deckhouse/lib-ssh

move from dhctl
Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

pkg/ssh/clissh/command.go
@@ -1,0 +1,196 @@
+// Copyright 2021 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clissh
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/deckhouse/lib-connection/pkg/settings"
+	"github.com/deckhouse/lib-connection/pkg/ssh/clissh/cmd"
+	"github.com/deckhouse/lib-connection/pkg/ssh/clissh/process"
+	"github.com/deckhouse/lib-connection/pkg/ssh/session"
+	"github.com/deckhouse/lib-connection/pkg/ssh/utils"
+)
+
+type Command struct {
+	settings settings.Settings
+
+	*process.Executor
+
+	Session *session.Session
+
+	Name string
+	Args []string
+	Env  []string
+
+	SSHArgs []string
+
+	onCommandStart func()
+
+	cmd *exec.Cmd
+}
+
+func NewCommand(sett settings.Settings, sess *session.Session, name string, arg ...string) *Command {
+	args := make([]string, len(arg))
+	copy(args, arg)
+	for i := range args {
+		if !strings.HasPrefix(args[i], `"`) &&
+			!strings.HasSuffix(args[i], `"`) &&
+			strings.Contains(args[i], " ") {
+			args[i] = strconv.Quote(args[i])
+		}
+	}
+
+	executor := process.NewDefaultExecutor(
+		sett,
+		cmd.NewSSH(sett, sess).
+			WithCommand(name, args...).
+			Cmd(context.Background()),
+	)
+
+	return &Command{
+		Executor: executor,
+		Session:  sess,
+		Name:     name,
+		Args:     args,
+		Env:      os.Environ(),
+		settings: sett,
+	}
+}
+
+func (c *Command) WithSSHArgs(args ...string) {
+	c.SSHArgs = args
+}
+
+func (c *Command) OnCommandStart(fn func()) {
+	c.onCommandStart = fn
+}
+
+func (c *Command) Sudo(ctx context.Context) {
+	cmdLine := c.Name + " " + strings.Join(c.Args, " ")
+	sudoCmdLine := fmt.Sprintf(
+		`sudo -p SudoPassword -H -S -i bash -c 'echo SUDO-SUCCESS && %s'`,
+		cmdLine,
+	)
+
+	var args []string
+	args = append(args, c.SSHArgs...)
+	args = append(args, []string{
+		"-t", // allocate tty to auto kill remote process when ssh process is killed
+		"-t", // need to force tty allocation because of stdin is pipe!
+	}...)
+
+	c.cmd = cmd.NewSSH(c.settings, c.Session).
+		WithArgs(args...).
+		WithCommand(sudoCmdLine).Cmd(ctx)
+
+	c.Executor = process.NewDefaultExecutor(c.settings, c.cmd)
+
+	c.WithMatchers(
+		utils.NewByteSequenceMatcher("SudoPassword"),
+		utils.NewByteSequenceMatcher("SUDO-SUCCESS").WaitNonMatched(),
+	)
+	c.OpenStdinPipe()
+
+	passSent := false
+	c.WithMatchHandler(func(pattern string) string {
+		logger := c.settings.Logger()
+		if pattern == "SudoPassword" {
+			var becomePass string
+
+			if c.Session.BecomePass != "" {
+				becomePass = c.Session.BecomePass
+			}
+			if !passSent {
+				// send pass through stdin
+				logger.DebugLn("Send become pass to cmd")
+				_, _ = c.Executor.Stdin.Write([]byte(becomePass + "\n"))
+				passSent = true
+			} else {
+				// Second prompt is error!
+				logger.ErrorLn("Bad sudo password")
+				// sending wrong password again will raise an error in process.Run()
+				_, _ = c.Executor.Stdin.Write([]byte(becomePass + "\n"))
+				// os.Exit(1)
+			}
+			return "reset"
+		}
+		if pattern == "SUDO-SUCCESS" {
+			logger.DebugLn("Got SUCCESS")
+			if c.onCommandStart != nil {
+				c.onCommandStart()
+			}
+			return "done"
+		}
+		return ""
+	})
+}
+
+func (c *Command) Cmd(ctx context.Context) {
+	c.cmd = cmd.NewSSH(c.settings, c.Session).
+		WithArgs(c.SSHArgs...).
+		WithCommand(c.Name, c.Args...).Cmd(ctx)
+
+	c.Executor = process.NewDefaultExecutor(c.settings, c.cmd)
+}
+
+func (c *Command) Output(ctx context.Context) ([]byte, []byte, error) {
+	if c.Session == nil {
+		return nil, nil, fmt.Errorf("Execute command %s: SSH client is undefined", c.Name)
+	}
+
+	c.cmd = cmd.NewSSH(c.settings, c.Session).
+		WithArgs(c.SSHArgs...).
+		WithCommand(c.Name, c.Args...).Cmd(ctx)
+
+	output, err := c.cmd.Output()
+	if err != nil {
+		return output, nil, fmt.Errorf("Execute command '%s': %w", c.Name, err)
+	}
+	return output, nil, nil
+}
+
+func (c *Command) CombinedOutput(ctx context.Context) ([]byte, error) {
+	if c.Session == nil {
+		return nil, fmt.Errorf("Execute command %s: sshClient is undefined", c.Name)
+	}
+
+	c.cmd = cmd.NewSSH(c.settings, c.Session).
+		//	//WithArgs().
+		WithCommand(c.Name, c.Args...).Cmd(ctx)
+
+	output, err := c.cmd.CombinedOutput()
+	if err != nil {
+		return output, fmt.Errorf("Execute command '%s': %w", c.Name, err)
+	}
+	return output, nil
+}
+
+func (c *Command) WithTimeout(timeout time.Duration) {
+	c.Executor = c.Executor.WithTimeout(timeout)
+}
+
+func (c *Command) WithEnv(env map[string]string) {
+	c.Env = make([]string, 0, len(env))
+	for k, v := range env {
+		c.Env = append(c.Env, fmt.Sprintf("%s=%s", k, v))
+	}
+}

pkg/ssh/clissh/reverse-tunnel.go
@@ -1,0 +1,266 @@
+// Copyright 2024 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clissh
+
+import (
+	"context"
+	"fmt"
+	"math/rand/v2"
+	"os"
+	"os/exec"
+	"sync"
+	"time"
+
+	connection "github.com/deckhouse/lib-connection/pkg"
+	"github.com/deckhouse/lib-connection/pkg/settings"
+	"github.com/deckhouse/lib-connection/pkg/ssh/clissh/cmd"
+	"github.com/deckhouse/lib-connection/pkg/ssh/session"
+	"github.com/deckhouse/lib-dhctl/pkg/retry"
+)
+
+type tunnelWaitResult struct {
+	id  int
+	err error
+}
+
+type ReverseTunnel struct {
+	settings settings.Settings
+
+	Session *session.Session
+	Address string
+
+	tunMutex sync.Mutex
+	sshCmd   *exec.Cmd
+	started  bool
+	stopCh   chan struct{}
+
+	errorCh chan tunnelWaitResult
+}
+
+func NewReverseTunnel(sett settings.Settings, sess *session.Session, address string) *ReverseTunnel {
+	return &ReverseTunnel{
+		settings: sett,
+		Session:  sess,
+		Address:  address,
+		errorCh:  make(chan tunnelWaitResult),
+	}
+}
+
+func (t *ReverseTunnel) Up() error {
+	_, err := t.upNewTunnel(-1)
+	return err
+}
+
+func (t *ReverseTunnel) upNewTunnel(oldId int) (int, error) {
+	logger := t.settings.Logger()
+	t.tunMutex.Lock()
+	defer t.tunMutex.Unlock()
+
+	if t.started {
+		logger.DebugF("[%d] Reverse tunnel already up\n", oldId)
+		return -1, fmt.Errorf("already up")
+	}
+
+	id := rand.Int()
+
+	logger.DebugF("[%d] Start reverse tunnel\n", id)
+
+	t.sshCmd = cmd.NewSSH(t.settings, t.Session).
+		WithArgs(
+			"-N", // no command
+			"-n", // no stdin
+			"-R", t.Address,
+		).
+		WithExitWhenTunnelFailure(true).
+		Cmd(context.Background())
+
+	err := t.sshCmd.Start()
+	if err != nil {
+		return id, fmt.Errorf("[%d] Cannot start tunnel ssh command: %w", id, err)
+	}
+
+	go func(localCmd *exec.Cmd, localID int) {
+		if localCmd == nil {
+			logger.ErrorF("[%d] sshCmd is nil before Wait()\n", localID)
+
+			t.errorCh <- tunnelWaitResult{
+				id:  localID,
+				err: fmt.Errorf("cannot Wait(): sshCmd is nil"),
+			}
+
+			return
+		}
+
+		logger.DebugF("[%d] Reverse tunnel started. Waiting for tunnel to stop...\n", localID)
+
+		err := localCmd.Wait()
+
+		t.errorCh <- tunnelWaitResult{
+			id:  localID,
+			err: err,
+		}
+
+		logger.DebugF("[%d] Reverse tunnel was stopped and handled\n", localID)
+	}(t.sshCmd, id)
+
+	t.started = true
+
+	return id, nil
+}
+
+func (t *ReverseTunnel) isStarted() bool {
+	t.tunMutex.Lock()
+	defer t.tunMutex.Unlock()
+	r := t.started
+	return r
+}
+
+func (t *ReverseTunnel) tryToRestart(ctx context.Context, id int, killer connection.ReverseTunnelKiller) (int, error) {
+	t.stop(id, false)
+	logger := t.settings.Logger()
+	logger.DebugF("[%d] Kill tunnel\n", id)
+	if out, err := killer.KillTunnel(ctx); err != nil {
+		logger.DebugF("[%d] Kill tunnel was finished with error: %v; stdout: '%s'\n", id, err, out)
+		return id, err
+	}
+	return t.upNewTunnel(id)
+}
+
+func (t *ReverseTunnel) StartHealthMonitor(ctx context.Context, checker connection.ReverseTunnelChecker, killer connection.ReverseTunnelKiller) {
+	t.tunMutex.Lock()
+	t.stopCh = make(chan struct{})
+	t.tunMutex.Unlock()
+
+	logger := t.settings.Logger()
+
+	checkReverseTunnel := func(id int) bool {
+
+		logger.DebugF("[%d] Start Check reverse tunnel\n", id)
+
+		err := retry.NewSilentLoop("Check reverse tunnel", 2, 2*time.Second).RunContext(ctx, func() error {
+			out, err := checker.CheckTunnel(ctx)
+			if err != nil {
+				logger.DebugF("[%d] Cannot check ssh tunnel: '%v': stderr: '%s'\n", id, err, out)
+				return err
+			}
+
+			return nil
+		})
+
+		if err != nil {
+			logger.DebugF("[%d] Tunnel check timeout, last error: %v\n", id, err)
+			return false
+		}
+
+		logger.DebugF("[%d] Tunnel check successful!\n", id)
+		return true
+	}
+
+	go func() {
+		logger.DebugLn("Start health monitor")
+		// we need chan for restarting because between restarting we can get stop signal
+		restartCh := make(chan int, 1024)
+		id := -1
+		restartsCount := 0
+		restart := func(id int) {
+			logger.DebugF("[%d] Send restart signal\n", id)
+			restartCh <- id
+			logger.DebugF("[%d] Signal was sent. Chan len: %d\n", id, len(restartCh))
+		}
+		for {
+
+			if !checkReverseTunnel(id) {
+				go restart(id)
+			}
+
+			select {
+			case <-t.stopCh:
+				logger.DebugLn("Stop health monitor")
+				return
+			case oldId := <-restartCh:
+				restartsCount++
+				logger.DebugF("[%d] Restart signal was received: restarts count %d\n", oldId, restartsCount)
+
+				if restartsCount > 1024 {
+					panic("Reverse tunnel restarts count exceeds 1024")
+				}
+
+				newId, err := t.tryToRestart(ctx, oldId, killer)
+				if err != nil {
+					logger.DebugF("[%d] Restart failed with error: %v\n", oldId, err)
+					go restart(oldId)
+					continue
+				}
+				logger.DebugF("[%d] Restart successful. New id %d\n", oldId, newId)
+				id = newId
+				restartsCount = 0
+			case err := <-t.errorCh:
+				id = err.id
+				logger.DebugF("[%d] Tunnel was stopped with error '%v'. Try restart fully\n", id, err.err)
+				started := t.isStarted()
+				if started {
+					logger.DebugF("[%d] Tunnel already up. Skip restarting\n", id)
+					continue
+				}
+
+				go restart(id)
+				continue
+			}
+		}
+	}()
+}
+
+func (t *ReverseTunnel) Stop() {
+	t.stop(-1, true)
+}
+
+func (t *ReverseTunnel) stop(id int, full bool) {
+	t.tunMutex.Lock()
+	defer t.tunMutex.Unlock()
+
+	logger := t.settings.Logger()
+
+	if !t.started {
+		logger.DebugF("[%d] Reverse tunnel already stopped\n", id)
+		return
+	}
+
+	logger.DebugF("[%d] Stop reverse tunnel\n", id)
+	defer logger.DebugF("[%d] End stop reverse tunnel\n", id)
+
+	if full && t.stopCh != nil {
+		logger.DebugF("[%d] Stop reverse tunnel health monitor\n", id)
+		t.stopCh <- struct{}{}
+	}
+
+	logger.DebugF("[%d] Try to find tunnel process %d\n", id, t.sshCmd.Process.Pid)
+	_, err := os.FindProcess(t.sshCmd.Process.Pid)
+	if err == nil {
+		logger.DebugF("[%d] Process found %d. Kill it\n", id, t.sshCmd.Process.Pid)
+		err := t.sshCmd.Process.Kill()
+		if err != nil {
+			logger.DebugF("[%d] Cannot kill process %d: %v\n", id, t.sshCmd.Process.Pid, err)
+		}
+	} else {
+		logger.DebugF("[%d] Stopping tunnel. Process %d already finished\n", id, t.sshCmd.Process.Pid)
+	}
+
+	t.sshCmd = nil
+	t.started = false
+}
+
+func (t *ReverseTunnel) String() string {
+	return fmt.Sprintf("%s:%s", "R", t.Address)
+}

deckhouse/lib-connection

pkg/ssh/clissh/command.go
@@ -1,0 +1,196 @@
+// Copyright 2021 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clissh
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/deckhouse/lib-connection/pkg/settings"
+	"github.com/deckhouse/lib-connection/pkg/ssh/clissh/cmd"
+	"github.com/deckhouse/lib-connection/pkg/ssh/clissh/process"
+	"github.com/deckhouse/lib-connection/pkg/ssh/session"
+	"github.com/deckhouse/lib-connection/pkg/ssh/utils"
+)
+
+type Command struct {
+	settings settings.Settings
+
+	*process.Executor
+
+	Session *session.Session
+
+	Name string
+	Args []string
+	Env  []string
+
+	SSHArgs []string
+
+	onCommandStart func()
+
+	cmd *exec.Cmd
+}
+
+func NewCommand(sett settings.Settings, sess *session.Session, name string, arg ...string) *Command {
+	args := make([]string, len(arg))
+	copy(args, arg)
+	for i := range args {
+		if !strings.HasPrefix(args[i], `"`) &&
+			!strings.HasSuffix(args[i], `"`) &&
+			strings.Contains(args[i], " ") {
+			args[i] = strconv.Quote(args[i])
+		}
+	}
+
+	executor := process.NewDefaultExecutor(
+		sett,
+		cmd.NewSSH(sett, sess).
+			WithCommand(name, args...).
+			Cmd(context.Background()),
+	)
+
+	return &Command{
+		Executor: executor,
+		Session:  sess,
+		Name:     name,
+		Args:     args,
+		Env:      os.Environ(),
+		settings: sett,
+	}
+}
+
+func (c *Command) WithSSHArgs(args ...string) {
+	c.SSHArgs = args
+}
+
+func (c *Command) OnCommandStart(fn func()) {
+	c.onCommandStart = fn
+}
+
+func (c *Command) Sudo(ctx context.Context) {
+	cmdLine := c.Name + " " + strings.Join(c.Args, " ")
+	sudoCmdLine := fmt.Sprintf(
+		`sudo -p SudoPassword -H -S -i bash -c 'echo SUDO-SUCCESS && %s'`,
+		cmdLine,
+	)
+
+	var args []string
+	args = append(args, c.SSHArgs...)
+	args = append(args, []string{
+		"-t", // allocate tty to auto kill remote process when ssh process is killed
+		"-t", // need to force tty allocation because of stdin is pipe!
+	}...)
+
+	c.cmd = cmd.NewSSH(c.settings, c.Session).
+		WithArgs(args...).
+		WithCommand(sudoCmdLine).Cmd(ctx)
+
+	c.Executor = process.NewDefaultExecutor(c.settings, c.cmd)
+
+	c.WithMatchers(
+		utils.NewByteSequenceMatcher("SudoPassword"),
+		utils.NewByteSequenceMatcher("SUDO-SUCCESS").WaitNonMatched(),
+	)
+	c.OpenStdinPipe()
+
+	passSent := false
+	c.WithMatchHandler(func(pattern string) string {
+		logger := c.settings.Logger()
+		if pattern == "SudoPassword" {
+			var becomePass string
+
+			if c.Session.BecomePass != "" {
+				becomePass = c.Session.BecomePass
+			}
+			if !passSent {
+				// send pass through stdin
+				logger.DebugF("Send become pass to cmd")
+				_, _ = c.Executor.Stdin.Write([]byte(becomePass + "\n"))
+				passSent = true
+			} else {
+				// Second prompt is error!
+				logger.ErrorF("Bad sudo password")
+				// sending wrong password again will raise an error in process.Run()
+				_, _ = c.Executor.Stdin.Write([]byte(becomePass + "\n"))
+				// os.Exit(1)
+			}
+			return "reset"
+		}
+		if pattern == "SUDO-SUCCESS" {
+			logger.DebugF("Got SUCCESS")
+			if c.onCommandStart != nil {
+				c.onCommandStart()
+			}
+			return "done"
+		}
+		return ""
+	})
+}
+
+func (c *Command) Cmd(ctx context.Context) {
+	c.cmd = cmd.NewSSH(c.settings, c.Session).
+		WithArgs(c.SSHArgs...).
+		WithCommand(c.Name, c.Args...).Cmd(ctx)
+
+	c.Executor = process.NewDefaultExecutor(c.settings, c.cmd)
+}
+
+func (c *Command) Output(ctx context.Context) ([]byte, []byte, error) {
+	if c.Session == nil {
+		return nil, nil, fmt.Errorf("Execute command %s: SSH client is undefined", c.Name)
+	}
+
+	c.cmd = cmd.NewSSH(c.settings, c.Session).
+		WithArgs(c.SSHArgs...).
+		WithCommand(c.Name, c.Args...).Cmd(ctx)
+
+	output, err := c.cmd.Output()
+	if err != nil {
+		return output, nil, fmt.Errorf("Execute command '%s': %w", c.Name, err)
+	}
+	return output, nil, nil
+}
+
+func (c *Command) CombinedOutput(ctx context.Context) ([]byte, error) {
+	if c.Session == nil {
+		return nil, fmt.Errorf("Execute command %s: sshClient is undefined", c.Name)
+	}
+
+	c.cmd = cmd.NewSSH(c.settings, c.Session).
+		//	//WithArgs().
+		WithCommand(c.Name, c.Args...).Cmd(ctx)
+
+	output, err := c.cmd.CombinedOutput()
+	if err != nil {
+		return output, fmt.Errorf("Execute command '%s': %w", c.Name, err)
+	}
+	return output, nil
+}
+
+func (c *Command) WithTimeout(timeout time.Duration) {
+	c.Executor = c.Executor.WithTimeout(timeout)
+}
+
+func (c *Command) WithEnv(env map[string]string) {
+	c.Env = make([]string, 0, len(env))
+	for k, v := range env {
+		c.Env = append(c.Env, fmt.Sprintf("%s=%s", k, v))
+	}
+}

pkg/ssh/clissh/reverse-tunnel.go
@@ -1,0 +1,265 @@
+// Copyright 2024 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clissh
+
+import (
+	"context"
+	"fmt"
+	"math/rand/v2"
+	"os"
+	"os/exec"
+	"sync"
+	"time"
+
+	"github.com/deckhouse/lib-dhctl/pkg/retry"
+
+	connection "github.com/deckhouse/lib-connection/pkg"
+	"github.com/deckhouse/lib-connection/pkg/settings"
+	"github.com/deckhouse/lib-connection/pkg/ssh/clissh/cmd"
+	"github.com/deckhouse/lib-connection/pkg/ssh/session"
+)
+
+type tunnelWaitResult struct {
+	id  int
+	err error
+}
+
+type ReverseTunnel struct {
+	settings settings.Settings
+
+	Session *session.Session
+	Address string
+
+	tunMutex sync.Mutex
+	sshCmd   *exec.Cmd
+	started  bool
+	stopCh   chan struct{}
+
+	errorCh chan tunnelWaitResult
+}
+
+func NewReverseTunnel(sett settings.Settings, sess *session.Session, address string) *ReverseTunnel {
+	return &ReverseTunnel{
+		settings: sett,
+		Session:  sess,
+		Address:  address,
+		errorCh:  make(chan tunnelWaitResult),
+	}
+}
+
+func (t *ReverseTunnel) Up() error {
+	_, err := t.upNewTunnel(-1)
+	return err
+}
+
+func (t *ReverseTunnel) upNewTunnel(oldId int) (int, error) {
+	logger := t.settings.Logger()
+	t.tunMutex.Lock()
+	defer t.tunMutex.Unlock()
+
+	if t.started {
+		logger.DebugF("[%d] Reverse tunnel already up\n", oldId)
+		return -1, fmt.Errorf("already up")
+	}
+
+	id := rand.Int()
+
+	logger.DebugF("[%d] Start reverse tunnel\n", id)
+
+	t.sshCmd = cmd.NewSSH(t.settings, t.Session).
+		WithArgs(
+			"-N", // no command
+			"-n", // no stdin
+			"-R", t.Address,
+		).
+		WithExitWhenTunnelFailure(true).
+		Cmd(context.Background())
+
+	err := t.sshCmd.Start()
+	if err != nil {
+		return id, fmt.Errorf("[%d] Cannot start tunnel ssh command: %w", id, err)
+	}
+
+	go func(localCmd *exec.Cmd, localID int) {
+		if localCmd == nil {
+			logger.ErrorF("[%d] sshCmd is nil before Wait()\n", localID)
+
+			t.errorCh <- tunnelWaitResult{
+				id:  localID,
+				err: fmt.Errorf("cannot Wait(): sshCmd is nil"),
+			}
+
+			return
+		}
+
+		logger.DebugF("[%d] Reverse tunnel started. Waiting for tunnel to stop...\n", localID)
+
+		err := localCmd.Wait()
+
+		t.errorCh <- tunnelWaitResult{
+			id:  localID,
+			err: err,
+		}
+
+		logger.DebugF("[%d] Reverse tunnel was stopped and handled\n", localID)
+	}(t.sshCmd, id)
+
+	t.started = true
+
+	return id, nil
+}
+
+func (t *ReverseTunnel) isStarted() bool {
+	t.tunMutex.Lock()
+	defer t.tunMutex.Unlock()
+	r := t.started
+	return r
+}
+
+func (t *ReverseTunnel) tryToRestart(ctx context.Context, id int, killer connection.ReverseTunnelKiller) (int, error) {
+	t.stop(id, false)
+	logger := t.settings.Logger()
+	logger.DebugF("[%d] Kill tunnel\n", id)
+	if out, err := killer.KillTunnel(ctx); err != nil {
+		logger.DebugF("[%d] Kill tunnel was finished with error: %v; stdout: '%s'\n", id, err, out)
+		return id, err
+	}
+	return t.upNewTunnel(id)
+}
+
+func (t *ReverseTunnel) StartHealthMonitor(ctx context.Context, checker connection.ReverseTunnelChecker, killer connection.ReverseTunnelKiller) {
+	t.tunMutex.Lock()
+	t.stopCh = make(chan struct{})
+	t.tunMutex.Unlock()
+
+	logger := t.settings.Logger()
+
+	checkReverseTunnel := func(id int) bool {
+		logger.DebugF("[%d] Start Check reverse tunnel\n", id)
+
+		err := retry.NewSilentLoop("Check reverse tunnel", 2, 2*time.Second).RunContext(ctx, func() error {
+			out, err := checker.CheckTunnel(ctx)
+			if err != nil {
+				logger.DebugF("[%d] Cannot check ssh tunnel: '%v': stderr: '%s'\n", id, err, out)
+				return err
+			}
+
+			return nil
+		})
+
+		if err != nil {
+			logger.DebugF("[%d] Tunnel check timeout, last error: %v\n", id, err)
+			return false
+		}
+
+		logger.DebugF("[%d] Tunnel check successful!\n", id)
+		return true
+	}
+
+	go func() {
+		logger.DebugF("Start health monitor")
+		// we need chan for restarting because between restarting we can get stop signal
+		restartCh := make(chan int, 1024)
+		id := -1
+		restartsCount := 0
+		restart := func(id int) {
+			logger.DebugF("[%d] Send restart signal\n", id)
+			restartCh <- id
+			logger.DebugF("[%d] Signal was sent. Chan len: %d\n", id, len(restartCh))
+		}
+		for {
+			if !checkReverseTunnel(id) {
+				go restart(id)
+			}
+
+			select {
+			case <-t.stopCh:
+				logger.DebugF("Stop health monitor")
+				return
+			case oldId := <-restartCh:
+				restartsCount++
+				logger.DebugF("[%d] Restart signal was received: restarts count %d\n", oldId, restartsCount)
+
+				if restartsCount > 1024 {
+					panic("Reverse tunnel restarts count exceeds 1024")
+				}
+
+				newId, err := t.tryToRestart(ctx, oldId, killer)
+				if err != nil {
+					logger.DebugF("[%d] Restart failed with error: %v\n", oldId, err)
+					go restart(oldId)
+					continue
+				}
+				logger.DebugF("[%d] Restart successful. New id %d\n", oldId, newId)
+				id = newId
+				restartsCount = 0
+			case err := <-t.errorCh:
+				id = err.id
+				logger.DebugF("[%d] Tunnel was stopped with error '%v'. Try restart fully\n", id, err.err)
+				started := t.isStarted()
+				if started {
+					logger.DebugF("[%d] Tunnel already up. Skip restarting\n", id)
+					continue
+				}
+
+				go restart(id)
+				continue
+			}
+		}
+	}()
+}
+
+func (t *ReverseTunnel) Stop() {
+	t.stop(-1, true)
+}
+
+func (t *ReverseTunnel) stop(id int, full bool) {
+	t.tunMutex.Lock()
+	defer t.tunMutex.Unlock()
+
+	logger := t.settings.Logger()
+
+	if !t.started {
+		logger.DebugF("[%d] Reverse tunnel already stopped\n", id)
+		return
+	}
+
+	logger.DebugF("[%d] Stop reverse tunnel\n", id)
+	defer logger.DebugF("[%d] End stop reverse tunnel\n", id)
+
+	if full && t.stopCh != nil {
+		logger.DebugF("[%d] Stop reverse tunnel health monitor\n", id)
+		t.stopCh <- struct{}{}
+	}
+
+	logger.DebugF("[%d] Try to find tunnel process %d\n", id, t.sshCmd.Process.Pid)
+	_, err := os.FindProcess(t.sshCmd.Process.Pid)
+	if err == nil {
+		logger.DebugF("[%d] Process found %d. Kill it\n", id, t.sshCmd.Process.Pid)
+		err := t.sshCmd.Process.Kill()
+		if err != nil {
+			logger.DebugF("[%d] Cannot kill process %d: %v\n", id, t.sshCmd.Process.Pid, err)
+		}
+	} else {
+		logger.DebugF("[%d] Stopping tunnel. Process %d already finished\n", id, t.sshCmd.Process.Pid)
+	}
+
+	t.sshCmd = nil
+	t.started = false
+}
+
+func (t *ReverseTunnel) String() string {
+	return fmt.Sprintf("%s:%s", "R", t.Address)
+}